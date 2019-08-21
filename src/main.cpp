a1bc0e71a6110511c874f9fa841b26d143c71a97
Hizkias Abraham <ahizkias@gmail.com>
2019-08-21 06:21:42 +0300

    created the project

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+// #include<boost/filesystem.hpp>
+
+#include "../include/hello.h"
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	// sayHello();
+	vector<string> v;
+	cout << "Hello Easy C++ project!" << endl;
+	
+	
+}
+
+class Database
+{
+private:
+	/* data */
+public:
+	Database(/* args */);
+	~Database();
+	void showTables();
+	void createTable();
+};
+
+Database::Database(/* args */)
+{
+}
+
+Database::~Database()
+{
+}
+
+class Table
+{
+private:
+	/* data */
+public:
+	Table(/* args */);
+	~Table();
+	void insert();
+	void update();
+	void deleteArow();
+};
+
+Table::Table(/* args */)
+{
+}
+
+Table::~Table()
+{
+}
